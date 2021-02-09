d53ce6e6b7c25246566c7b9c711128832780c3aa
codeKingMike <codekingmike@gmail.com>
2021-02-08 22:55:45 -0800

    made a full rest API

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const Posts = require('../models/Post');
+const Post = require('../models/Post');
 
-router.get('/', (req, res) => {
-    res.send('We are posts');
+// GET BACK A POST
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
 
+// SUBMITS A POST
 router.post('/', async (req, res) => {
-    const post = new Posts({
+    const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
@@ -17,8 +24,37 @@ router.post('/', async (req, res) => {
     } catch (err) {
         res.json({ message: err });
     }
-
 });
 
+// GET BACK A SPECIFIC POST
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// DELETE A POST
+router.delete('/:postId', async (req, res) => {
+    try {
+        const removedPost = await Post.remove({ _id: req.params.postId });
+        res.json(removedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+});
+
+// UPDATE A POST
+router.patch('/:postId', async (req, res) => {
+    try {
+        const updatedPost = await Post.updateOne({ _id: req.params.postId }, { $set: { title: req.body.title } });
+        res.json(updatedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
+
+});
 
 module.exports = router;

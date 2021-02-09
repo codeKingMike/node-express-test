87b02fbdbabef0cec2309371002038287c195302
codeKingMike <codekingmike@gmail.com>
2021-02-08 20:50:14 -0800

    Learning postman and connecting to db

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express');
-
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
+
+
+app.use(bodyParser.json());
+// Import Router
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
 
 // ROUTES
 app.get('/', (req, res) => {
     res.send('We are home');
 });
 
-app.get('/posts', (req, res) => {
-    res.send('We are posts');
-});
-
 
 // Connect to DB
-mongoose.connect('mongodb+srv://jakesmith21310:Helloworld44@cluster0.qkyk6.mongodb.net/test-db?retryWrites=true&w=majority', () => console.log('connected to DB!'))
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => console.log('connected to DB!'))
 
 app.listen(3000);
